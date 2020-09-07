5ca8c82e47cf68088800227e33ff6ca92c5b1385
Amritanshu Sikdar <amritanshu.sikdar@gmail.com>
2020-09-07 20:43:06 +0530

    add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div>
+            <b-navbar toggleable="lg" type="dark" variant="dark">
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav>
+                        <b-nav-item>
+                            <i class="material-icons">dehaze</i>
+                        </b-nav-item>
+                    </b-navbar-nav>
+
+                    <b-navbar-nav class="mx-auto">
+                        <b-navbar-brand>VioDev</b-navbar-brand>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-navbar>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Navbar",
+};
+</script>
+
+<style>
+@import "https://fonts.googleapis.com/icon?family=Material+Icons";
+</style>
