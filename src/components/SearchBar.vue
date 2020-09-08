2180e7fd434c5543d043050e0b7579e91e60e914
Udit Karode <udit.karode@gmail.com>
2020-09-08 17:13:28 +0530

    kek its done

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -21,8 +21,13 @@ export default {
         convert: () => {
             var currentpost = document.getElementById('newformatf').value;
 
-            /* remove the photo tag that might sneak in */
+            /* remove a tag that might sneak in */
             currentpost = currentpost.replace("[ Photo ]\n", "");
+            currentpost = currentpost.replace("[ Video ]\n", "");
+            currentpost = currentpost.replace("[ GIF ]\n", "");
+
+            /* warn for removal of changelog */
+            currentpost = currentpost.replace(/Changelog:/, "CHANGE ME HEREE Changelog:");
 
             /* move hashtags to the start */ 
             var hashtags = currentpost.match(/.*?$/);
@@ -35,12 +40,15 @@ export default {
             var size = currentpost.match(/Size: (.*)/);
             var updated = currentpost.match(/Updated: (.*)/);
             var by = currentpost.match(/By: (.*)/);
+            var support = currentpost.match(/Support Group: ?(.*)/);
 
             /* strip new format detail items */
-            currentpost = currentpost.replace(new RegExp(status[0] + "\n?"), "");
-            currentpost = currentpost.replace(new RegExp("\n?" + size[0] + "\n?"), "");
-            currentpost = currentpost.replace(new RegExp("\n?" + updated[0] + "\n?"), "");
+            currentpost = currentpost.replace(new RegExp(status[0]), "");
+            currentpost = currentpost.replace(new RegExp("\n?" + size[0]), "");
+            currentpost = currentpost.replace(new RegExp("\n?" + updated[0]), "");
             currentpost = currentpost.replace(new RegExp("\n?" + by[0]), "");
+            currentpost = currentpost.replace(new RegExp("\n?" + support[0]), "");
+
             /* bruh this bracket shit is gay af */
             currentpost = currentpost.replace(new RegExp("\n?" + androidv[0].replace("(", "\\(").replace(")", "\\)")), "");
 
@@ -59,7 +67,7 @@ export default {
             currentpost = currentpost.split("\n");
             var clAt = -1;
             for(var i=0; i<parseInt(currentpost.length); i++){
-                if(currentpost[i].includes("Credit")){
+                if(currentpost[i].includes("Credit") || currentpost[i].includes("Special Thanks")){
                     clAt = i;
                     break;
                 }
@@ -72,6 +80,24 @@ export default {
 
             currentpost = currentpost.join("\n");
 
+            /* replace channel and group links */
+            currentpost = currentpost.replace("Updates Channel: @RedmiNote7ProUpdates", "**By** " + by[1]);
+            currentpost = currentpost.replace("Main Group: @VioletDiscussion", "**Follow* @VioletOfficialUpdates\n**Join** @VioletOfficialChat");
+
+            /* add link placeholders */
+            currentpost = currentpost.split("\n");
+            var last = parseInt(currentpost.length)-1;
+            while(!currentpost[last] == "") last -= 1;
+            currentpost.splice(last, 0, "▪️ [Support](https://t.me/" + support[1].replace("@", "") + ")");
+            currentpost.splice(last, 0, "▪️ [Device Changelog](PUTLINKHERE)");
+            currentpost.splice(last, 0, "▪️ [Source Changelog](PUTLINKHERE)");
+            currentpost.splice(last, 0, "▪️ [Download](PUTLINKHERE) [" + size[1] + "]");
+            currentpost = currentpost.join("\n");
+
+            /* clean up duplicate newlines */
+            currentpost = currentpost.replace(/\n\n/, "\n");
+
+            /* finally apply the converted format to the text field */
             document.getElementById('newformatf').value = currentpost;
         }
     }
