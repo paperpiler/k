a6875c41a04ae38b56385f9137bec5bf7e2d526f
Udit Karode <udit.karode@gmail.com>
2020-09-08 16:12:23 +0530

    some done kek

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -6,6 +6,7 @@
             style="font-family: Source Code Pro"
             placeholder="Enter post..."
             class="rsearch"
+            id="newformatf"
         />
     </div>
 
@@ -18,7 +19,62 @@ export default {
     name: "SearchBar",
     methods: {
         convert: () => {
-            alert('bruh working!');
+            var currentpost = document.getElementById('newformatf').value;
+
+            /* remove the photo tag that might sneak in */
+            currentpost = currentpost.replace("[ Photo ]\n", "");
+
+            /* move hashtags to the start */ 
+            var hashtags = currentpost.match(/.*?$/);
+            currentpost = currentpost.replace(new RegExp("\n?\n?" + hashtags), "");
+            currentpost = hashtags + "\n" + currentpost;
+
+            /* fetch rom specific details */
+            var status = currentpost.match(/Status: (.*)/);
+            var androidv = currentpost.match(/Android: (.*)/);
+            var size = currentpost.match(/Size: (.*)/);
+            var updated = currentpost.match(/Updated: (.*)/);
+            var by = currentpost.match(/By: (.*)/);
+
+            /* strip new format detail items */
+            currentpost = currentpost.replace(new RegExp(status[0] + "\n?"), "");
+            currentpost = currentpost.replace(new RegExp("\n?" + size[0] + "\n?"), "");
+            currentpost = currentpost.replace(new RegExp("\n?" + updated[0] + "\n?"), "");
+            currentpost = currentpost.replace(new RegExp("\n?" + by[0]), "");
+            /* bruh this bracket shit is gay af */
+            currentpost = currentpost.replace(new RegExp("\n?" + androidv[0].replace("(", "\\(").replace(")", "\\)")), "");
+
+            /* create the new header */
+            currentpost = currentpost.split("\n");
+            var newhead = "**" + currentpost[1] + " - " + status[1] + " | " + androidv[0].replace(":", "").replace(/\([A-Z]?[a-z]?\)/, "").replace(new RegExp(" $"), "") + "**";
+            currentpost[1] = newhead;
+            currentpost = currentpost.join("\n");
+
+            /* insert update date */
+            currentpost = currentpost.split("\n");
+            currentpost.splice(2, 0, "**Updated:** " + "__" + updated[1] + "__");
+            currentpost = currentpost.join("\n");
+
+            /* remove changelog section 
+            currentpost = currentpost.split("\n");
+            var clAt = -1;
+            alert(currentpost.length());
+            for(var i=0; i<currentpost.length(); i++){
+                alert(currentpost[i]);
+                if(currentpost[i].includes("Changelog")){
+                    alert("uau@")
+                    clAt = i;
+                    break;
+                }
+            }
+
+            if(clAt != -1){
+                currentpost.splice(clAt, 1);
+            }
+
+            currentpost = currentpost.join("\n");*/
+
+            document.getElementById('newformatf').value = currentpost;
         }
     }
 };
